37d44e320cea6be77b80f8f8352036622d52af5f
Irfan5208 <setia1924@gmail.com>
2024-05-23 20:06:04 +0700

    first commit Jarvis about Laravel

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perkenalan Diri</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        .header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .profile-img {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            margin: 0 auto;
+            display: block;
+        }
+
+        .intro {
+            text-align: justify;
+        }
+
+        .social-links {
+            list-style: none;
+            padding: 0;
+            margin: 20px 0;
+        }
+
+        .social-links li {
+            display: inline-block;
+            margin: 0 10px;
+        }
+
+        .social-links a {
+            text-decoration: none;
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <h1>Perkenalan Diri</h1>
+    </div>
+
+    <img src="irfan.png" alt="Foto Profil" class="profile-img">
+
+    <p class="intro">
+        Hai semuanya! Nama saya Irfan Setiawan.
+        Panggil saja saya Irfan.
+        Saya sangat senang bisa bergabung di kelas JarvisCamp ini!
+
+
+        Silahkan hubungi saya melalui:
+    </p>
+
+    <ul class="social-links" align="center">
+        <li><a href="https://www.facebook.com/isdm1/" target="_blank">Facebook</a></li>
+        <li><a href="https://www.instagram.com/irfan.520809/" target="_blank">Instagram</a></li>
+        <li><a href="https://github.com/Irfan5208/" target="_blank">GitHub</a></li>
+    </ul>
+</div>
+</body>
+</html>
